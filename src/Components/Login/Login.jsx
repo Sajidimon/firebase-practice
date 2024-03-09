8c2c3f172a77c1d8759c246d972a51cff9a2cfaa
sajidimon <imonchowdhury6@gmail.com>
2024-03-09 21:05:40 +0600

    github authentication

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
+
+    const githubprovider = new GithubAuthProvider();
 
     const [user, setUser] = useState(null)
     const auth = getAuth(app);
@@ -33,10 +35,23 @@ const Login = () => {
             })
     }
 
+    const handlegithublogin = () => {
+        signInWithPopup(auth, githubprovider)
+            .then(result => {
+                const Loggedinuser = result.user;
+                console.log(Loggedinuser)
+                setUser(Loggedinuser);
+        })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
 
     return (
         <div>
             <button onClick={handleGoogleSignin}>Login here</button>
+            <button onClick={handlegithublogin}>Github Login</button>
             <button onClick={handlesignout}>Signout</button>
             <h3>User name is: {user?.displayName}</h3>
             <h3>Users email is: {user?.email}</h3>
